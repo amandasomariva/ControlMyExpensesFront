8fdd6ab81e4ae94dbc6fdcabf81c2061ae48a72e
kelly.cardoso <kelly.cardoso@betha.com.br>
2021-12-11 15:53:00 -0300

    commit da kelly dia 11dezembro

--- Template/public/js/controllers/home.controller.js
+++ Template/public/js/controllers/home.controller.js
@@ -3,26 +3,36 @@
 
     angular.module("MyApp").controller("HomeController", HomeController);
 
-    HomeController.$inject = ["$rootScope", "$location", "$window"];
+    HomeController.$inject = ["$rootScope", "$location", "$window","GastoService","InvestimentoService","RendaService","ResumoService"];
 
-    function HomeController($rootScope, $location, $window) {
+    function HomeController($rootScope, $location, $window, GastoService, InvestimentoService, RendaService, ResumoService) {
         var vm = this;
         var itemSelecionado = -1;
 
-        
-        
-        vm.investimentosPage = investimentosPage;
+        m.investimentosPage = investimentosPage;
         vm.rendasPage = rendasPage;
         vm.resumosPage = resumosPage;
         vm.opcionaisPage = opcionaisPage;
         vm.gastosPage = gastosPage;
         vm.usuariosPage = usuariosPage;
         vm.isAdministrador = isAdministrador;
-        
+        vm.valorTotal = 0;
+        vm.saldo = 0;
+        vm.valorGasto = 0;
 
         activate();
 
-         
+        function activate() {
+            GastoService.find().then(function(result) {
+                vm.valor = result.data;
+                vm.valorTotal = vm.valor.length;
+            });
+            RendaService.find().then(function(result) {
+                vm.saldo = result.data;
+                vm.saldo = vm.saldo.length;
+                vm.valorGasto = vm.saldo - vm.valorGasto;
+            });
+        }
 
         function isAdministrador (){
             return $window.localStorage.administrador;
